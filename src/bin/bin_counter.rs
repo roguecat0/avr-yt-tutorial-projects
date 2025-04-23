b6f76db21c0ddae02f3f5804c3b2439e2a553c62
roguecat0 <roguecat0@gmail.com>
2025-04-23 20:59:32 +0200

    improved bin counter with generics

--- src/bin/bin_counter.rs
+++ src/bin/bin_counter.rs
@@ -1,7 +1,21 @@
 #![no_main]
 #![no_std]
 
+use arduino_hal::port::PinOps;
+use avr_hal_generic::port::Pin;
+use avr_hal_generic::port::mode::Output;
+// use embedded_hal::digital::OutputPin;
+
 use panic_halt as _;
+
+#[inline]
+fn show_conditional<P: PinOps>(pin: &mut Pin<Output, P>, condition: bool) {
+    if condition {
+        let _ = pin.set_high();
+    } else {
+        let _ = pin.set_low();
+    }
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -15,21 +29,9 @@ fn main() -> ! {
     p2.set_low();
     p3.set_low();
     loop {
-        if number & 0b1 != 0 {
-            p1.set_high();
-        } else {
-            p1.set_low();
-        }
-        if number & 0b10 != 0 {
-            p2.set_high();
-        } else {
-            p2.set_low();
-        }
-        if number & 0b100 != 0 {
-            p3.set_high();
-        } else {
-            p3.set_low();
-        }
+        show_conditional(&mut p1, number & 0b1 != 0);
+        show_conditional(&mut p2, number & 0b10 != 0);
+        show_conditional(&mut p3, number & 0b100 != 0);
         arduino_hal::delay_ms(500);
         number += 1;
         if number >= 8 {
