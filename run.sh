0a2d2c585901cafc997de1cfc48d87e4daeb0db5
roguecat0 <roguecat0@gmail.com>
2025-04-24 20:32:56 +0200

    added serial in example (doesn't work...)

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 if [ "$RMODE" == "size" ]; then
   avr-size -C --mcu=atmega328p $@
+elif [ "$RMODE" == "sim" ]; then
+  simavr -m atmega328p $@
 else
   ravedude "$@"
 fi
---
+++ src/bin/serial_in.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use arduino_hal::prelude::*;
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap_infallible();
+
+    loop {
+        // Read a byte from the serial connection
+        let b = nb::block!(serial.read()).unwrap_infallible();
+
+        // Answer
+        ufmt::uwriteln!(&mut serial, "Got {}!\r", b - 48).unwrap_infallible();
+    }
+}
