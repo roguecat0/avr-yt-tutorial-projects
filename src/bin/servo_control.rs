ab3ea5ac6d8a5c1549e0a27e16eae9d4f93696e9
roguecat0 <roguecat0@gmail.com>
2025-04-26 23:26:21 +0200

    works with pin 10

--- src/bin/servo_control.rs
+++ src/bin/servo_control.rs
@@ -24,25 +24,36 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
 
     // Important because this sets the bit in the DDR register!
-    pins.d9.into_output();
+    // pins.d9.into_output();
+    pins.d10.into_output();
 
     // - TC1 runs off a 250kHz clock, with 5000 counts per overflow => 50 Hz signal.
     // - Each count increases the duty-cycle by 4us.
     // - Use OC1A which is connected to D9 of the Arduino Uno.
     let tc1 = dp.TC1;
     tc1.icr1.write(|w| w.bits(4999));
+    // if pin 9
+    // tc1.tccr1a
+    //     // .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
+    //     .write(|w| unsafe { w.bits(0b10000010) });
+    // tc1.tccr1b
+    //     // .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
+    //     .write(|w| unsafe { w.bits(0b00011011) });
+    // if pin 10
     tc1.tccr1a
-        // .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
-        .write(|w| unsafe { w.bits(0b10000010) });
+        .write(|w| w.wgm1().bits(0b10).com1b().match_clear());
     tc1.tccr1b
-        // .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
-        .write(|w| unsafe { w.bits(0b00011011) });
+        .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
 
     loop {
         // 100 counts => 0.4ms
         // 700 counts => 2.8ms
         for duty in 100..=700 {
-            tc1.ocr1a.write(|w| w.bits(duty));
+            // pin 9
+            // tc1.ocr1a.write(|w| w.bits(duty));
+            // pint 10
+            tc1.ocr1b.write(|w| w.bits(duty));
+
             arduino_hal::delay_ms(20);
         }
     }
